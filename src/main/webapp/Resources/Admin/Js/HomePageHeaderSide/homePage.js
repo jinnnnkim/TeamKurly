61df17d22f5fdb12e517c9699b0c4dfba470ba3b
jungahzzzang <jungahzzzang@gmail.com>
2022-02-10 19:25:09 +0900

    no message

--- src/main/webapp/Resources/Admin/Js/HomePageHeaderSide/homePage.js
+++ src/main/webapp/Resources/Admin/Js/HomePageHeaderSide/homePage.js
@@ -3,17 +3,17 @@
  */
 
 
-$(document).ready(function(){
+/*$(document).ready(function(){
         // memu 클래스 바로 하위에 있는 a 태그를 클릭했을때
        $(".menu>a").click(function(){
             //현재 클릭한 태그가 a 이기 때문에
             // a 옆의 태그중 ul 태그에 hide 클래스 태그를 숨겼다 펼쳤다 한다.
           $(this).next("ul").toggleClass("hide");
         });
- });
+ });*/
 
-function dropMenu_btn(){
-            let click = document.getElementById("drop_content");
+function dropMenu_btn1(){
+            let click = document.getElementById("drop_content1");
             if(click.style.display === "none"){
                 click.style.display = "block";
  
@@ -21,7 +21,15 @@ function dropMenu_btn(){
                 click.style.display = "none";
             }
         }
-
+function dropMenu_btn2(){
+            let click = document.getElementById("drop_content2");
+            if(click.style.display === "none"){
+                click.style.display = "block";
+ 
+            }else{
+                click.style.display = "none";
+            }
+        }
 
 	
 		
