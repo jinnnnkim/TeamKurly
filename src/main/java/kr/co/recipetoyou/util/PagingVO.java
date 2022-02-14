cfc0befb9980991cb4efbf0d93740029688d7964
itmdeveloper <choidasol95@gmail.com>
2022-02-15 02:23:28 +0900

    user_id 게터 세터 중복으로 인한 오류 수정

--- src/main/java/kr/co/recipetoyou/util/PagingVO.java
+++ src/main/java/kr/co/recipetoyou/util/PagingVO.java
@@ -177,14 +177,6 @@ public class PagingVO {
 			this.title = title;
 		}
 
-		public String getUser_id() {
-			return user_id;
-		}
-
-		public void setUser_id(String user_id) {
-			this.user_id = user_id;
-		}
-
 		public String getContent() {
 			return content;
 		}
