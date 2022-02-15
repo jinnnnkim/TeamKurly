b540c481650203d4da29f0391deed8c4a737b485
Selenbp1 <akfkdhwk@gmail.com>
2022-02-16 01:51:37 +0900

    관리자 유저 목록 검색 및 페이징 기능 구현 중 80%

--- src/main/java/kr/co/recipetoyou/admin/aduser/AdUserService.java
+++ src/main/java/kr/co/recipetoyou/admin/aduser/AdUserService.java
@@ -13,7 +13,7 @@ public interface AdUserService {
 	public List<AdUserVO> listUsers(PagingVO vo) throws Exception;
 			
 	//회원 수 조회
-	public int userCount() throws DataAccessException;
+	public int userCount(PagingVO vo) throws DataAccessException;
 			
 	//회원 상세 정보 조회
 	public AdUserVO getUserInfo(String id) throws DataAccessException;
